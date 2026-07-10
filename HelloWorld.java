d100a118c522087fd8222953233c52faa3ae46bf
praisemb458 <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-10 15:25:39 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// Write a simple program to display a message
-
-public class HelloWorld {
-    public static void main(String[] args)
-    {
-
-        System.out.println("Hello, World!");
-    }
-}
