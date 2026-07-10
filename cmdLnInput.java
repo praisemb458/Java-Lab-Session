814c392577c020ad8b81a93e9884b19e92d7a57f
praisemb458 <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-10 14:52:07 +0530

    Add files via upload

---
+++ cmdLnInput.java
@@ -0,0 +1,13 @@
+// write a java program to display arithmetic operations
+
+class cmdLnInput
+{
+	public static void main(String args[])
+	
+	{
+	int a= Integer.parseInt(args[0]);
+	int b= Integer.parseInt(args[1]);
+	int sum= a + b
+	System.out.println("Argument:" +args[0]);
+	}
+}
---
+++ hello.java
@@ -0,0 +1,9 @@
+// write a java program to display name from user.
+
+class hello
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
