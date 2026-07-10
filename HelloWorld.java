eff3ab3c8b28696e68b68fe0f010f7318fe1fa8a
praisemb458 <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-10 14:45:54 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Write a simple program to display a message
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello, World!");
+    }
+}
