3917617e6009dcd07c7a246d8077e21460b841ff
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-15 14:41:34 +0530

    Add files via upload

---
+++ program6_4.java
@@ -0,0 +1,11 @@
+public class program6_4 {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count <= 5); 
+        
+        System.out.println("Loop executed");
+    }
+}
