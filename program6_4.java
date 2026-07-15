33bab44dd6ce66e70c26dbcc5e273c02c0298c5c
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-15 14:45:54 +0530

    Add files via upload

---
+++ program6_4.java
@@ -0,0 +1,14 @@
+// Write a program to execute DO WHILE Loop //
+
+
+public class program6_4 {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count <= 5); 
+        
+        System.out.println("Loop executed");
+    }
+}
