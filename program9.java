6d953b522820afb85aaef5f6cb882f3a4b48af06
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-17 15:46:00 +0530

    Add files via upload

---
+++ program9.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class program9 {
+
+    public static void main(String args[]) {
+    Scanner input = new Scanner (System.in);
+
+   System.out.println("Enter your GR No. :");
+   int GR = input.nextLine();
+
+   System.out.println("Enter your name :);
+   String name = input.nextLine();
+
+   System.out.println("Enter subject : ");
+   String name = input.nextLine();
+
+if (a<=100||a>=90) {
+
+System.out.println("Outstanding Performance!");
+System.out.println("Grade: O");
+}
+else if (a<=89||a>=80) {
+
+System.out.println("Excellent Work!");
+System.out.println("Grade: A+");
+}
+
+else if (a<=79||a>=70) {
+
+System.out.println("Very Good Work!");
+System.out.println("Grade :A);
+}
+
+else if (a<=69||a>=60) { 
+
+System.out.println("Good Work!");  
+System.out.println("Grade: B+);
+}
+
+else if (a<=59||a>=50) {
+
+System.out.println("Good!");
+System.out.println("Grade: B");
+}
+
+else if (a<=49||a>=40) {
+
+System.out.println("Getting better!);
+System.out.println("Grade: C");
+}
+
+else if (a<=39||a>=30) {
+
+System.out.println("You need to do better!");
+System.out.println("Grade: D");
+}
+
+else if (a<=29||a>=0) {
+
+System.out.println("You're cooked!");
+System.out.println("Grade: F");
+      }
+  }
+
+
+}
+
+ 
