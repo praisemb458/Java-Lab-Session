63b620349df65558fde5d944f163bc3e9a486b99
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-07-24 14:43:16 +0530

    Add files via upload

---
+++ program9_2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class program9_2 {
+
+    public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Here is the * pattern");
+    System.out.print("Enter your number :");
+     
+    int n = input.nextInt();
+
+    int i,j;
+
+         {
+          for (i = 1; i <= 5; i++) {
+            
+          for (j = 1; j <= i; n++) {
+          System.out.print("*"); 
+            }
+
+            System.out.println(""); }
+        
+    }
+  }
+
+}
